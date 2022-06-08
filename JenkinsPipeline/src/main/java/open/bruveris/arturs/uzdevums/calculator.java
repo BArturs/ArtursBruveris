c4ab17f9809fbff61a5ef47277a0f4784fca061c
BArturs <101707790+BArturs@users.noreply.github.com>
2022-06-08 20:59:50 +0300

    Update calculator.java

--- JenkinsPipeline/src/main/java/open/bruveris/arturs/uzdevums/calculator.java
+++ JenkinsPipeline/src/main/java/open/bruveris/arturs/uzdevums/calculator.java
@@ -2,25 +2,26 @@ package open.bruveris.arturs.uzdevums;
 import java.util.Scanner;
 
 public class calculator {
+    // Sum method for the calculator
     static int sum(int n1, int n2) {
         return n1 + n2;
     }
-
+    // Sub method for the calculator
     static int sub(int n1, int n2) {
         return n1 - n2;
     }
-
+    // Mult method for the calculator
     static int multiply(int n1, int n2) {
         return n1 * n2;
     }
-
+    // Div method for the calculator
     static int div(int n1, int n2) {
         return n1 / n2;
     }
 
     public static void main(String args[]) {
-        Scanner scNum = new Scanner(System.in);
-        Scanner scTask = new Scanner(System.in);
+        Scanner scNum = new Scanner(System.in); // Scanner for numbers
+        Scanner scTask = new Scanner(System.in); // Scanner for the task of calculator
 
         System.out.print("Input first number: ");
         int n1 = scNum.nextInt();
@@ -30,7 +31,7 @@ public class calculator {
         String taskInput = scTask.nextLine();
         String taskForSwitch = taskInput.toLowerCase();
 
-        switch (taskForSwitch) {
+        switch (taskForSwitch) { // Catches the taskForSwitch text and does the appropriate task
             case "summation" -> System.out.println("The summation of " + n1 + " and " + n2 + " is " + sum(n1, n2));
             case "subtraction" -> System.out.println("The subtraction of " + n2 + " from " + n1 + " is " + sub(n1, n2));
             case "multiplication" -> System.out.println("The multiplication of " + n1 + " times " + n2 + " is " + multiply(n1, n2));
